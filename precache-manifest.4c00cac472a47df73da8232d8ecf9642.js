3bc6661b127ba35218383cfc82ae996b67e1086f
Brittany Boese <bboese04@gmail.com>
2019-10-31 10:23:43 -0700

    Initial Commit
    Adding project

---
+++ precache-manifest.4c00cac472a47df73da8232d8ecf9642.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0b95998b468ce6c71ca2aa9475ed98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e0ebb781fe97d36eb5d",
+    "url": "./static/css/main.15a23eef.chunk.css"
+  },
+  {
+    "revision": "b2fe0869f6f20c5cd8d8",
+    "url": "./static/js/2.3e617622.chunk.js"
+  },
+  {
+    "revision": "1e0ebb781fe97d36eb5d",
+    "url": "./static/js/main.98ef84f7.chunk.js"
+  },
+  {
+    "revision": "f224d8ed1e2f6f1de73d",
+    "url": "./static/js/runtime-main.2b1918bf.js"
+  }
+]);
---
+++ precache-manifest.f3965c59fd10e9287c7f19a91f5fdd1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e87c136a721c4509847b940b57a484",
+    "url": "./index.html"
+  },
+  {
+    "revision": "312909e7698115b43810",
+    "url": "./static/css/main.e71fb744.chunk.css"
+  },
+  {
+    "revision": "41ba12bd9060397b2229",
+    "url": "./static/js/2.42f742ca.chunk.js"
+  },
+  {
+    "revision": "312909e7698115b43810",
+    "url": "./static/js/main.75942f91.chunk.js"
+  },
+  {
+    "revision": "f224d8ed1e2f6f1de73d",
+    "url": "./static/js/runtime-main.2b1918bf.js"
+  }
+]);
